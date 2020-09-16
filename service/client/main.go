a16d77c36110b1a8b31d77174c3b340e015cc506
kezunhao <kezunhao@yeah.net>
2020-09-16 17:43:20 +0800

    init

--- service/client/main.go
+++ service/client/main.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	// 建立连接到gRPC服务
-	conn, err := grpc.Dial("10.254.211.78:8028", grpc.WithInsecure())
+	conn, err := grpc.Dial("GRPC_K8S_SERVER_PORT", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
